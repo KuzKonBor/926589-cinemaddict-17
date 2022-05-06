94e60f8ad080c94f336ca904ca465da93ef980bf
KuzKonBor <82975463+KuzKonBor@users.noreply.github.com>
2022-05-06 22:13:54 +0300

    Отрисовал элементы сайта

---
+++ src/view/filters-view.js
@@ -0,0 +1,56 @@
+import {createElement} from '../render.js';
+
+const createFiltersNavigationViewTemplate = () =>
+  (`<nav class="main-navigation">
+  <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+  <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+  <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+  <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+</nav>`
+  );
+class FiltersNavigationView {
+  getTemplate() {
+    return createFiltersNavigationViewTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
+
+
+const createFiltersSortViewTemplate = () =>
+  (`
+<ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>`
+  );
+class FiltersSortView {
+  getTemplate() {
+    return createFiltersSortViewTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
+
+export {FiltersNavigationView, FiltersSortView};
